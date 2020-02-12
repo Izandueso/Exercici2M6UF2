ab685478fb713ac6341ece28ab1cbb44931f5afd
Alumne <Alumne@104-PC11>
2020-02-12 20:24:16 +0100

    fIRSTcOMIT

---
+++ src/InsertDataDemo.java
@@ -0,0 +1,48 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
+
+public class InsertDataDemo {
+	public static void main(String[] args) throws SQLException {
+		Connection connection = null;
+		Statement stmt = null;
+		
+		String stm = "INSERT INTO ALUMNES (DNI, NOM, DATA_NAIXEMENT, ADREÇA, CODI_POSTAL) VALUES('6472849P', 'IZAN', '1992-12-12', 'CARRER','43840')";
+		
+		String stm2 = "INSERT INTO ALUMNES (DNI, NOM, DATA_NAIXEMENT, ADREÇA, CODI_POSTAL) VALUES('6402849P', 'ggggg', '1992-12-12', 'CARRER','43840')";
+		
+		
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager
+					.getConnection("jdbc:mysql://localhost:3306/alumnes", "root", "");
+
+			stmt = connection.createStatement();
+			
+			connection.setAutoCommit(false);
+			stmt = connection.createStatement();
+			stmt.executeUpdate(stm);
+			stmt.executeUpdate(stm2);
+			connection.commit();
+			connection.setAutoCommit(true);
+			
+		} 
+		catch (Exception e) {
+			connection.rollback();
+			System.out.println("" + e.getMessage());
+		}finally {
+			try {
+				stmt.close();
+				connection.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
